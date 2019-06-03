5383c5cf72a44405fe30fa39e338605fbe5d29c2
alex-mont <44395427+alex-mont@users.noreply.github.com>
2019-06-03 16:54:14 +0200

    register

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  result = '';
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm){
+    if (!form.valid) {
+      return;
+    }
+    //to do
+  }
+
+  onRegister(form: NgForm){
+    console.log(form);
+    this.router.navigateByUrl('/home');
   }
 
 }
