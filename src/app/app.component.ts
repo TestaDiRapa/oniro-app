b185235023afe78db4542eda1f92dabc00d38936
AntonioBho <lafeminaantonio@libero.it>
2019-06-05 10:00:04 +0200

    logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, MenuController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,9 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private router: Router,
+    private menu: MenuController
   ) {
     this.initializeApp();
   }
@@ -22,5 +25,11 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  onLogout() {
+        this.router.navigateByUrl('/authentication');
+        this.menu.enable(false);
+
   }
 }
