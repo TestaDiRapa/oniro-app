5321215a2e48bc6a7856053a76e0067d3da47dfd
Vicodin <a.nappi32@studenti.unisa.it>
2019-06-06 21:42:47 +0200

    maps: a super-alpha release

--- src/app/home/gmaps/gmaps.page.ts
+++ src/app/home/gmaps/gmaps.page.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import {
+  Geolocation
+} from '@ionic-native/geolocation/ngx';
+
+ 
+declare var google;
 
 @Component({
   selector: 'app-gmaps',
@@ -6,10 +12,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gmaps.page.scss'],
 })
 export class GmapsPage implements OnInit {
+  map: any;
+  @ViewChild('map') mapElement: ElementRef;
 
-  constructor() { }
+  constructor(
+    private geolocation: Geolocation
+  ) { }
 
   ngOnInit() {
+    this.loadMap();
   }
 
+  loadMap() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      let latLng = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+ 
+      this.getAddressFromCoords(resp.coords.latitude, resp.coords.longitude);
+ 
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+ 
+      this.map.addListener('tilesloaded', () => {
+        console.log('accuracy',this.map);
+        this.getAddressFromCoords(this.map.center.lat(), this.map.center.lng())
+      });
+ 
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+  }
+
+  getAddressFromCoords(lattitude, longitude) {
+    console.log("getAddressFromCoords "+lattitude+" "+longitude);
+
+}
 }
