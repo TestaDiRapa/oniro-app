2af0abcd51ef54304a3bbb36a347e76346c48e3f
AntonioBho <lafeminaantonio@libero.it>
2019-06-06 14:04:04 +0200

    login Paziente|Medico dal server

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -3,6 +3,9 @@ import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { SegmentChangeEventDetail } from '@ionic/core';
 import { AuthenticationService } from '../services/authentication.service';
+import { UserService } from '../services/userService.service';
+import { Paziente } from '../register/paziente.model';
+import { Medico } from '../register/medico.model';
 
 @Component({
   selector: 'app-login',
@@ -13,9 +16,8 @@ export class LoginPage implements OnInit {
   isUser = true;
   private username: string;
   private password: string;
-  private goOn = false;
 
-  constructor(private router: Router, private auth: AuthenticationService) { }
+  constructor(private router: Router, private auth: AuthenticationService, private user: UserService) { }
 
   ngOnInit() {
   }
@@ -46,8 +48,17 @@ export class LoginPage implements OnInit {
     this.auth.login(this.username, this.password, true).subscribe(res => {
       console.log(res);
       if (res.status === 'ok') {
+        if (this.isUser) {
+          this.user.setUser(new Paziente(res.message['name'], res.message['surname'], null ,
+           res.message['phone_server'], res.message['email'], res.message['_id'], res.message['age']));
+        } else {
+          this.user.setUser(new Medico(res.message['name'], res.message['surname'], null,
+           res.message['phone'], res.message['email'], res.message['_id'], res.message['address']));
+        }
+        console.log(this.user.getUser());
         this.auth.isAuthenticated = true;
         this.router.navigateByUrl('/home');
+
 
       } else {
         console.log(res.message);
