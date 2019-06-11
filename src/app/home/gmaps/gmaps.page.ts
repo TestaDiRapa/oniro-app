ddf3aa8b1d1577911b8bac4f89f1dcce8a7b0e6f
Vicodin <a.nappi32@studenti.unisa.it>
2019-06-11 09:10:47 +0200

    Habemus mappas

--- src/app/home/gmaps/gmaps.page.ts
+++ src/app/home/gmaps/gmaps.page.ts
@@ -2,7 +2,20 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { MenuController } from '@ionic/angular';
 import { GetCoordService } from './get-coord.service';
-
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  Environment,
+  Geocoder,
+  GeocoderResult,
+  LocationService,
+  MyLocation
+} from '@ionic-native/google-maps';
 declare var google;
 
 @Component({
@@ -12,8 +25,9 @@ declare var google;
 })
 export class GmapsPage implements OnInit {
   @ViewChild('map') mapElement: ElementRef;
-  map: any;
+  map: GoogleMap;
   doctors: any;
+  addresses: string [] ;
 
   constructor(
     private geolocation: Geolocation,
@@ -23,11 +37,12 @@ export class GmapsPage implements OnInit {
 
   ngOnInit() {
     this.menuCtrl.toggle();
-    this.loadMap();
+    this.getCoordinates();
   }
 
   loadMap() {
-    this.geolocation.getCurrentPosition().then((resp) => {
+    //const myPosition = this.geolocation.getCurrentPosition();
+ /*   this.geolocation.getCurrentPosition().then((resp) => {
       const latLng = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
       const mapOptions = {
         center: latLng,
@@ -35,10 +50,50 @@ export class GmapsPage implements OnInit {
         mapTypeId: google.maps.MapTypeId.ROADMAP
       };
       this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+      let marker: Marker
     }).catch((error) => {
       console.log('Error getting location', error);
+    });**/
+    let opzioni: GoogleMapOptions;
+    LocationService.getMyLocation().then((myLocation: MyLocation) => {
+
+// tslint:disable-next-line: label-position
+     /* opzioni = {
+        camera: {
+          target: myLocation.latLng,
+          zoom: 50
+        },
+      }*/
+      this.map = GoogleMaps.create('map');
+      let marker :Marker = this.map.addMarkerSync({
+        'position': myLocation.latLng,
+        'title': 'Hai aggiunto un Marker'
+      });
+      this.map.animateCamera({
+        target: marker.getPosition(),
+        zoom:17
+      }).then(() => {
+        marker.showInfoWindow();
+      })
+  }); 
+   /* for (const doctor of this.doctors) {
+      this.addresses.push(doctor['address']);
+    }*/
+    
+   /* const marker: Marker = this.map.addMarkerSync({
+      title: 'Ionic',
+      icon: 'blue',
+      animation: 'DROP',
+      position: {
+        lat: 43.0741904,
+        lng: -89.3809802
+      }
     });
-  }
+    marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+      alert('clicked');
+    });*/
+    
+}
 
   getCoordinates() {
     console.log('sono qui');
@@ -50,5 +105,4 @@ export class GmapsPage implements OnInit {
       });
     });
   }
-
 }
