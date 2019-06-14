7142ca6ebbf5324587996fda7f2d4889ed83a007
AntonioBho <lafeminaantonio@libero.it>
2019-06-14 14:09:38 +0200

    mega push href

--- src/app/home/diary/diary.module.ts
+++ src/app/home/diary/diary.module.ts
@@ -7,7 +7,6 @@ import { IonicModule } from '@ionic/angular';
 
 import { DiaryPage } from './diary.page';
 import { GoogleChartsModule } from 'angular-google-charts';
-import { DiaryModalComponent } from './diary-modal/diary-modal.component';
 
 const routes: Routes = [
   {
@@ -24,7 +23,6 @@ const routes: Routes = [
     GoogleChartsModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [DiaryPage, DiaryModalComponent],
-  entryComponents: [DiaryModalComponent]
+  declarations: [DiaryPage]
 })
 export class DiaryPageModule {}
