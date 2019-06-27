e2eb40dd6562ef2e70e8a4fd76f16f94260efa68
TestaDiRapa <v.c.pierro@gmail.com>
2019-06-27 14:18:01 +0200

    Added some documentation

--- src/app/services/bluetooth/bluetooth-device.model.ts
+++ src/app/services/bluetooth/bluetooth-device.model.ts
@@ -1,3 +1,7 @@
+/**
+ * This class models a Bluetooth device, that is identified by a name and a MAC address
+ * (or UUID for iOS devices)
+ */
 export class BluetoothDevice {
 
     constructor(
--- src/app/services/bluetooth/bluetooth.service.ts
+++ src/app/services/bluetooth/bluetooth.service.ts
@@ -3,6 +3,10 @@ import { BluetoothSerial } from '@ionic-native/bluetooth-serial/ngx';
 import { BluetoothDevice } from './bluetooth-device.model';
 import { Storage } from '@ionic/storage';
 
+
+/**
+ * This service manages the bluetooth connection between the application and the device
+ */
 @Injectable({
   providedIn: 'root'
 })
@@ -14,10 +18,19 @@ export class BluetoothService {
     private storage: Storage
   ) { }
 
+  /**
+   * Returns if the Bluetooth is enabled
+   * @returns {boolean} true if the device enabled bluetooth, false otherwise
+   */
   isEnabled() {
     return this.bluetooth.isEnabled();
   }
 
+  /**
+   * Check if the application is already paired with a device or if has a device stored in 
+   * memory and returns it
+   * @returns {Promise<BluetoothDevice>} a promise containing the device
+   */
   get device() {
     if (!this.bluetoothDevice) {
       return this.storage.get('device').then(stringObject => {
@@ -37,6 +50,11 @@ export class BluetoothService {
     });
   }
 
+  /**
+   * This method receive the name and the address of a Bluetooth device and stores it in memory
+   * @param name the device name
+   * @param address the device address
+   */
   addDevice(name: string, address: string) {
     this.bluetoothDevice = new BluetoothDevice(name, address);
     this.storage.set('device', JSON.stringify(this.bluetoothDevice));
