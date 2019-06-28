b35350f1c35c6abf2542ddf044735d57c08e33b6
Vicodin <a.nappi32@studenti.unisa.it>
2019-06-28 11:12:53 +0200

    Update register.page.ts

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -28,7 +28,7 @@ export class RegisterPage implements OnInit {
   );
   cf: RegExp = new RegExp(
 // tslint:disable-next-line: max-line-length
-    /^(?:[A-Z][AEIOU][AEIOUX]|[B-DF-HJ-NP-TV-Z]{2}[A-Z]){2}(?:[\dLMNP-V]{2}(?:[A-EHLMPR-T](?:[04LQ][1-9MNP-V]|[15MR][\dLMNP-V]|[26NS][0-8LMNP-U])|[DHPS][37PT][0L]|[ACELMRT][37PT][01LM]|[AC-EHLMPR-T][26NS][9V])|(?:[02468LNQSU][048LQU]|[13579MPRTV][26NS])B[26NS][9V])(?:[A-MZ][1-9MNP-V][\dLMNP-V]{2}|[A-M][0L](?:[1-9MNP-V][\dLMNP-V]|[0L][1-9MNP-V]))[A-Z]$/i
+/^[A-Za-z]{6}[0-9LMNPQRSTUV]{2}[A-Za-z]{1}[0-9LMNPQRSTUV]{2}[A-Za-z]{1}[0-9LMNPQRSTUV]{3}[A-Za-z]{1}$/
   );
   constructor(
     private router: Router,
